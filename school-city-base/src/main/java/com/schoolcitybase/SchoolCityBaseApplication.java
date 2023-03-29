93bb07fc42a42084d2fe6ffdeaf3c9ebfe7fc821
“fenke” <2191027216@qq.com>
2023-03-29 11:45:10 +0800

    父工程和基础工程

---
+++ school-city-base/src/main/java/com/schoolcitybase/SchoolCityBaseApplication.java
@@ -0,0 +1,13 @@
+package com.schoolcitybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCityBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolCityBaseApplication.class, args);
+    }
+
+}
---
+++ school-city-base/src/test/java/com/schoolcitybase/SchoolCityBaseApplicationTests.java
@@ -0,0 +1,12 @@
+package com.schoolcitybase;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCityBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
